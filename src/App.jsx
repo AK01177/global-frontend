999ad051ca141c76b7c9903eb0b9ada403443435
AK01177 <aryu7334@gmail.com>
2025-07-15 14:57:15 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -134,6 +134,7 @@ function App() {
           display: 'flex',
           alignItems: 'center',
           justifyContent: 'center',
+          paddingLeft: '0px'
         }}>
           <MapComponent
             onLocationSelect={handleLocationSelect}
